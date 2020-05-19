3f0768b914554806571536cf92136f7802561c60
Elchi DIana <47711461+daimondy@users.noreply.github.com>
2020-05-19 15:04:01 +0300

    3

--- level3/task1.java
+++ level3/task1.java
@@ -1,4 +1,17 @@
+import java.lang.Math.*;
+
 public class task1{
 	public static void main(String[] args){	
+		System.out.println(solutions(1, 0, 1));
+	}
+	public static int solutions(int a, int b, int c){
+		double ur = Math.pow(b, 2) - 4*a*c;
+		System.out.print("solutions: ");
+		if (ur>0)
+			return 2;
+		else if (ur==0) 
+			return 1;
+		else
+			return 0;
 	}
 }
